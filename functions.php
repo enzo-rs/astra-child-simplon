1c461c274a5c3e0cf6b500f10aede8946e501e89
enzo-rs <enzorojoall@gmail.com>
2021-03-12 10:23:48 +0100

    :sparkles: Add new color palette

--- functions.php
+++ functions.php
@@ -71,5 +71,34 @@ function astra_child_simplon_add_body_class( $classes ) {
 
 /* your code starts here 👇 */
 
+
+add_theme_support( 'editor-color-palette', array(
+	array(
+		'name'  => __( 'Carbon', 'textdomain' ),
+		'slug'  => 'carbon',
+		'color'	=> '#4A4A4A',
+	),
+  	array(
+		'name'	=> __( 'Caramel', 'textdomain' ),
+		'slug'	=> 'caramel',
+		'color'	=> '#8B572A',
+	),
+	array(
+		'name'  => __( 'Vanilla', 'textdomain' ),
+		'slug'  => 'vanilla',
+		'color'	=> '#EBE5D9',
+	),
+	array(
+		'name'  => __( 'Cappuccino', 'textdomain' ),
+		'slug'  => 'cappuccino',
+		'color'	=> '#B09F82',
+	),
+	array(
+		'name'  => __( 'White', 'textdomain' ),
+		'slug'  => 'white',
+		'color' => '#FFFFFF',
+	),
+) );
+
 // add custom color palette to the block editor
 
